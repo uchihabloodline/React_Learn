f19b7de54949ba4fa98535f7f7c35e63ae112995
shivam <geetapandey595@gmail.com>
2020-04-26 05:17:38 +0530

    CartItem code added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CartItem from './CartItem';
 
 function App() {
-  const name = 'Shivam'
-  const isLoggedIn = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {isLoggedIn?name:'World'}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CartItem/>
     </div>
   );
 }
