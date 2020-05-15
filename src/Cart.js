5bae30e58b8e1dafb217a0edf012ab289274f258
shivam <geetapandey595@gmail.com>
2020-05-16 01:35:08 +0530

    showing list using map

--- src/Cart.js
+++ src/Cart.js
@@ -4,8 +4,14 @@ import CartItem from './CartItem';
 class Cart extends React.Component {
     
     render(){
+        const arr = [1,2,3,4,5];
         return (
+           
             <div className='cart'>
+                
+                {arr.map(function(item){
+                    return item+4
+                })}
                 <div>
                     <CartItem />
                     <CartItem />
