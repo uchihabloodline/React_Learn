d4f284a6037f63f1e728c0dfa40ec18335230962
shivam <geetapandey595@gmail.com>
2020-06-28 01:09:49 +0530

    CartItem changed to function based component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,46 +1,43 @@
 import React from 'react';
 //import Cart from './Cart';
 
-class CartItem extends React.Component {
-
-  render () {
-    console.log("props-->", this.props);
-    const {price, title, qty} = this.props.product;
-    const {product, onDecreaseQuantity, onIncreaseQuantity, onDeleteProduct} = this.props;
-    return (
-      <div className="cart-item">
-        {this.props.jsx}
-        <div className="left-block">
-          <img style={styles.image} alt="" />
-        </div>
-        <div className="right-block">
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>Price:{price}</div>
-          <div style={ { color: '#777' } }>Qty:{qty}</div>
-          <div className="cart-item-actions">
-            {/* Buttons */}
-              < img alt="increase" 
-                className="action-icons" 
-                src="https://image.flaticon.com/icons/svg/992/992651.svg" 
-                onClick = {()=> onIncreaseQuantity(product)}
-              />
+const CartItem = (props) => {
+  console.log("props-->", props);
+  const {price, title, qty} = props.product;
+  const {product, onDecreaseQuantity, onIncreaseQuantity, onDeleteProduct} = props;
+  return (
+    <div className="cart-item">
+      {props.jsx}
+      <div className="left-block">
+        <img style={styles.image} alt="" />
+      </div>
+      <div className="right-block">
+        <div style={ { fontSize: 25 } }>{title}</div>
+        <div style={ { color: '#777' } }>Price:{price}</div>
+        <div style={ { color: '#777' } }>Qty:{qty}</div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+            < img alt="increase" 
+              className="action-icons" 
+              src="https://image.flaticon.com/icons/svg/992/992651.svg" 
+              onClick = {()=> onIncreaseQuantity(product)}
+            />
+            
+            < img alt="decrease"
+              className="action-icons" 
+              src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              onClick = {()=> onDecreaseQuantity(product)}  
+            />
+            
+            < img alt="delete" 
+              className="action-icons" 
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+              onClick = {()=> onDeleteProduct(product.id)} />
               
-              < img alt="decrease"
-                className="action-icons" 
-                src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
-                onClick = {()=> onDecreaseQuantity(product)}  
-              />
-              
-              < img alt="delete" 
-                className="action-icons" 
-                src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
-                onClick = {()=> onDeleteProduct(product.id)} />
-                
-          </div>
         </div>
       </div>
+    </div>
     );
-  }
 }
 
 const styles = {
